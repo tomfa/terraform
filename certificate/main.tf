387dbca880fbd4d40f898fba6a2fdf354b9dc5e7
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2020-10-14 10:29:09 +0200

    certificate: fix bug where validation_method var wasn't respected

--- certificate/main.tf
+++ certificate/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_acm_certificate" "cert" {
   domain_name       = var.domain
   subject_alternative_names = var.alternative_names
-  validation_method = "DNS"
+  validation_method = var.validation_method
 
   lifecycle {
     create_before_destroy = true
