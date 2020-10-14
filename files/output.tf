2af80ac2f62407b3639edaac5146a1b31285e21d
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2020-10-14 11:31:10 +0200

    files: output S3 upload url

--- files/output.tf
+++ files/output.tf
@@ -5,3 +5,7 @@ output "AWS_SECRET_ACCESS_KEY" {
 output "AWS_ACCESS_KEY_ID" {
   value = aws_iam_access_key.bucket_user.id
 }
+
+output "S3_FILES_UPLOAD_URL" {
+  value = aws_s3_bucket.bucket.bucket_regional_domain_name
+}
