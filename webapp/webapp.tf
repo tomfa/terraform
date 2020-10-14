cc22b535e763b227b74427d49fe37c5c82405cf3
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2020-10-14 11:29:09 +0200

    webapp: redirect http viewer to https

--- webapp/webapp.tf
+++ webapp/webapp.tf
@@ -106,7 +106,7 @@ resource "aws_cloudfront_distribution" "distribution" {
             }
       }
 
-        viewer_protocol_policy = "allow-all"
+        viewer_protocol_policy = "redirect-to-https"
         min_ttl                = 0
         default_ttl            = 3600
         max_ttl                = 86400
