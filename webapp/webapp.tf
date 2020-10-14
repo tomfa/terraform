fd804a65bc11406690e12f7d5de73f86c8df2408
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2020-10-14 14:17:57 +0200

    webapp: enable gzipping content

--- webapp/webapp.tf
+++ webapp/webapp.tf
@@ -107,6 +107,7 @@ resource "aws_cloudfront_distribution" "distribution" {
       }
 
         viewer_protocol_policy = "redirect-to-https"
+        compress               = true
         min_ttl                = 0
         default_ttl            = 3600
         max_ttl                = 86400
