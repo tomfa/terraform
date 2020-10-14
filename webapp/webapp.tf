3e5770f63e3d44e403d45d121418dca8e3399353
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2020-10-14 14:16:53 +0200

    webapp: fix issue with S3-original protocol
    The S3 website is not available on https.

--- webapp/webapp.tf
+++ webapp/webapp.tf
@@ -70,7 +70,7 @@ resource "aws_cloudfront_distribution" "distribution" {
         custom_origin_config  {
             http_port = 80
             https_port = 443
-            origin_protocol_policy = "https-only"
+            origin_protocol_policy = "http-only"
             origin_ssl_protocols = ["TLSv1", "TLSv1.1", "TLSv1.2"]
         }
     }
