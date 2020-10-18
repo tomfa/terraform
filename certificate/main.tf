2193f731c48c2611a336932ad6bb22bff79d5f26
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2020-10-18 11:16:51 +0200

    certificates: add dns validation pointers
    FIXES #2

--- certificate/main.tf
+++ certificate/main.tf
@@ -12,6 +12,19 @@ resource "aws_acm_certificate" "cert" {
   }
 }
 
+resource "aws_route53_record" "example" {
+  for_each = var.route53_zone_id != "" && var.validation_method == "DNS" ? aws_acm_certificate.cert.domain_validation_options : {}
+
+  allow_overwrite = true
+  name            = each.value.resource_record_name
+  records         = [each.value.resource_record_value]
+  ttl             = 60
+  type            = each.value.resource_record_type
+  zone_id         = var.route53_zone_id
+}
+
 resource "aws_acm_certificate_validation" "cert" {
   certificate_arn         = aws_acm_certificate.cert.arn
 }
+
+
--- certificate/variables.tf
+++ certificate/variables.tf
@@ -12,3 +12,9 @@ variable "validation_method" {
 	description = "How AWS should validate ownership, Email will require manual renewals. DNS recommended."
 	default = "DNS"
 }
+
+variable "route53_zone_id" {
+	description = "If specified along with validation method being DNS, the route53 zone id will be used to set up DNS pointers to validate the certificates"
+	default = ""
+	type = string
+}
