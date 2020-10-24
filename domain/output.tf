c3b342f1712de239c45b9f90303845d014a37da1
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2020-10-24 14:54:02 +0200

    domain: add output for AWS DNS servers
    Used to point registrar DNS pointers to Route53.

--- domain/output.tf
+++ domain/output.tf
@@ -1,3 +1,8 @@
 output "DOMAIN_ZONE_ID" {
   value = aws_route53_zone.domain.zone_id
 }
+
+output "DNS_SERVERS" {
+  description = "Point your registrar DNS config to point to these IPs"
+  value = aws_route53_zone.domain.name_servers
+}
