f1dae15307efc13862b107744633701e8ca17243
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2020-10-24 14:52:19 +0200

    certificate: add output for dns validation
    Needed if you want a certificate with DNS validation, but using
    other DNS servers.

--- certificate/output.tf
+++ certificate/output.tf
@@ -1,3 +1,8 @@
 output "CERTIFICATE_ARN" {
 	value = aws_acm_certificate_validation.cert.certificate_arn
 }
+
+output "CERTIFICATE_DNS_VALIDATION_POINTERS" {
+	description = "If you do NOT use AWS for DNS, add these pointers to your existing DNS server to validate the certificate."
+	value = aws_acm_certificate.cert.domain_validation_options
+}
