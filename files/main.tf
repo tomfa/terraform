cbf1c8253afa4c552fb604be03fd30b260cd5ed0
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2021-04-01 18:18:19 +0200

    files: allow setting files cors origins

--- files/main.tf
+++ files/main.tf
@@ -5,4 +5,12 @@ provider "aws" {
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
   acl = var.acl
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST", "GET", "HEAD", "DELETE"]
+    allowed_origins = var.cors_origins
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
 }
--- files/variables.tf
+++ files/variables.tf
@@ -2,6 +2,12 @@ variable "aws_region" {
 	description = "e.g. eu-north-1, us-east-1"
 }
 
+variable "cors_origins" {
+	description = "e.g. [\"www.mybucket.com\"] or [\"*\"]. Required for web page to interact with S3 bucket. By default allows any URL"
+	default = ["*"]
+	type = list(string)
+}
+
 variable "bucket_name" {
 	description = "e.g. your-bucket-name"
 }
